f53b5e86a3e1f048e9c242a4c0114fc3f6989c31
Vartika <bhardwajvartika9450@gmail.com>
2022-01-16 01:33:33 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -9,22 +9,39 @@ function App() {
 
   ])
   useEffect(() => {
-    const fetchTasks =async () => {
-      const res=await fetch('http://localhost:3000/tasks')
-      const data=await res.json()
-      console.log(data)
+    const getTasks=async()=>{
+      const tasksFromServer=await fetchTasks()
+      setTasks(tasksFromServer)
+
     }
-    fetchTasks()
-  })
+    getTasks()
+  },[])
+  const fetchTasks =async () => {
+    const res=await fetch('http://localhost:5000/tasks')
+    const data=await res.json()
+    return data
+  }
   
-  const addTask =(task) => {
-    const id=Math.random()
-    console.log(id)
-    const newTask= {id , ...task}
-    setTasks([...tasks,newTask])
+  const addTask =async(task) => {
+    const res =await fetch('http://localhost:5000/tasks',{
+      method:'POST',
+      headers:{
+        'Content-type':'application/json',
+      },
+      body:JSON.stringify(task)
+    })
+    // const id=Math.random()
+    // console.log(id)
+    // const newTask= {id , ...task}
+    const data=await res.json()
+    setTasks([...tasks,data])
 
   }
-  const deleteTask = (id) => {
+  const deleteTask = async(id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`,{
+      method : 'DELETE',
+    })
+
     setTasks(tasks.filter((task) => task.id !== id))
 
   }
